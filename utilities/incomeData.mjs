9fc7006384f48e2cfffb3a0f53595ea1f1661f61
Alka Sah <alkasah485@gmail.com>
2024-11-04 12:24:18 -0800

    modifications saved

---
+++ utilities/incomeData.mjs
@@ -0,0 +1,9 @@
+const incomeData = [
+    {title:'Grocery Shopping', category:'food', description:'weekly groceries', email:'john.doe@example.com'},
+    {title:'Salary', category:'income', description:'monthly salary', email:'jane.smith@example.com'},
+    {title:'Electricity Bill', category:'food', description:'weekly groceries', email:'john.doe@example.com'},
+    {title:'Internet Bill', category:'utilities', description:'monthly internet bill', email:'alice.doe@example.com'},
+
+];
+
+export default incomeData
