b95f5c782877b3d725a9f51e75c6e47715693de5
Alka Sah <alkasah485@gmail.com>
2024-11-05 11:22:04 -0800

    modification saved

---
+++ utilities/expenseData.mjs
@@ -0,0 +1,9 @@
+const expenseData = [
+    {title:'Grocery Shopping',amount : '10000', category:'food', description:'weekly groceries', email:'john.doe@example.com'},
+    {title:'Salary',amount : '50000', category:'income', description:'monthly salary', email:'jane.smith@example.com'},
+    {title:'Electricity Bill',amount : '1000', category:'food', description:'weekly groceries', email:'john.doe@example.com'},
+    {title:'Internet Bill',amount : '10000', category:'utilities', description:'monthly internet bill', email:'alice.doe@example.com'},
+
+];
+
+export default expenseData;
